a5e11beb5e30299dfbf166d65d4765c53d9c9e7c
Marek Węglarz <neuroxiq@wp.pl>
2020-04-04 16:11:45 +0200

    reloaded test application

---
+++ src/Arctium/DEBUG_ConsoleApplicationForTests/Program.cs
@@ -0,0 +1,34 @@
+﻿//
+// [Testing console program]
+// Testing console program exclude from solution.
+// Imports all Arctium dll, easy to check how something works
+//
+
+
+using Arctium.Cryptography.ASN1.ObjectSyntax.Types.BuildInTypes;
+using Arctium.Cryptography.ASN1.Serialization.X690;
+using Arctium.Cryptography.ASN1.Serialization.X690.DER;
+using Arctium.Cryptography.ASN1.Standards.X509.X509Cert;
+using Arctium.Cryptography.HashFunctions.Hashes;
+using Arctium.Shared.Helpers.Buffers;
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace DEBUG_ConsoleApplicationForTests
+{
+    class Program
+    {
+        static void Main()
+        {
+            
+        }
+
+        private static void ASDFG()
+        {
+            
+        }
+    }
+}
