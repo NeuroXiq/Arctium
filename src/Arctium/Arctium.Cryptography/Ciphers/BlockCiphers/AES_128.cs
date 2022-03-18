4e0d70ad929f1d04fa08d11cd52e569274869573
marek <marek>
2022-03-18 13:37:18 +0100

    aes 128 optimized

---
+++ src/Arctium/Arctium.Cryptography/Ciphers/BlockCiphers/AES_128.cs
@@ -0,0 +1,56 @@
+﻿using Arctium.Cryptography.Ciphers.BlockCiphers.Algorithms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arctium.Cryptography.Ciphers.BlockCiphers
+{
+    public unsafe class AES_128 : BlockCipher
+    {
+        public AESAlgorithm.Context context;
+
+        public AES_128(byte[] key) : base(key, 128)
+        {
+            context = AESAlgorithm.Initialize(key);
+        }
+
+        public override long Decrypt(byte[] input, long offset, byte[] output, long outputOffset, long length)
+        {
+            fixed(uint* k = &context.ExpandedKey[0])
+            fixed (byte* ip = &input[offset], op = &output[outputOffset])
+            {
+                byte* ipp = ip;
+                byte* opp = op;
+                for (long i = 0; i < length; i += 16)
+                {
+                    AES128Algorithm.DecryptBlock(k, ipp, opp);
+                    ipp += 16;
+                    opp += 16;
+                }
+            }
+
+            return length;
+        }
+
+        public override long Encrypt(byte[] input, long offset, byte[] output, long outputOffset, long length)
+        {
+            fixed(uint* key = &context.ExpandedKey[0])
+            fixed (byte* ip = &input[offset], op = &output[outputOffset])
+            {
+                byte* ipp = ip;
+                byte* opp = op;
+
+                for (long i = 0; i < length; i += 16)
+                {
+                    AES128Algorithm.EncryptBlock(key, ipp, opp);
+                    ipp += 16;
+                    opp += 16;
+                }
+            }
+
+            return length;
+        }
+    }
+}
---
+++ src/Arctium/Arctium.Cryptography/Ciphers/BlockCiphers/BlockCipherModeHelper.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Arctium.Cryptography.Ciphers.BlockCiphers
+{
+    internal unsafe class BlockCipherModeHelper
+    {
+        public delegate void EncryptBlock(byte* input, long inputOffset, byte* output, long outputOffset);
+
+        public BlockCipherModeHelper()
+        {
+
+        }
+    }
+}
