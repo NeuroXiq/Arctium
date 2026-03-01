4634d85e518872430ca9ab08e8306263b69850d6
neuroxiq <neuroxiq@wp.pl>
2026-03-01 17:48:54 +0100

    dns doh client

--- src/Arctium/Arctium/Protocol/DNS/DnsClientMessageIODoHRfc8484.cs
+++ src/Arctium/Arctium/Protocol/DNS/DnsClientMessageIODoHRfc8484.cs
@@ -5,23 +5,45 @@ namespace Arctium.Protocol.DNS
 {
     public class DnsClientMessageIODoHRfc8484 : IDnsClientMessageIO
     {
-        public readonly string ServerUri;
+        public readonly Func<DnsClientMessageIOArg, UriMethodResult> UriMethodSelect;
+        public readonly bool HttpsRequired;
+        public readonly HttpClient DnsHttpClient;
+        public readonly bool RequiredHttps;
 
-        public DnsClientMessageIODoHRfc8484(string serverUri)
+        public DnsClientMessageIODoHRfc8484(
+            Func<DnsClientMessageIOArg, UriMethodResult> uriMethodSelect,
+            HttpClient httpClient,
+            bool requiredHttps)
         {
-            if (string.IsNullOrWhiteSpace(serverUri)) throw new ArgumentException("serverUri is null or empty");
+            if (uriMethodSelect == null) throw new ArgumentNullException(nameof(uriMethodSelect));
+            if (httpClient == null) throw new ArgumentNullException(nameof(httpClient));
+            if (requiredHttps == null) throw new ArgumentNullException(nameof(requiredHttps));
 
-            ServerUri = serverUri;
+            UriMethodSelect = uriMethodSelect;
+            DnsHttpClient = httpClient;
+            RequiredHttps = requiredHttps;
         }
 
         public virtual Task<Message> QueryServerAsync(DnsClientMessageIOArg arg)
         {
-            throw new NotImplementedException();
+            UriMethodResult uriMethod = UriMethodSelect(arg);
+
+            if (string.IsNullOrWhiteSpace(uriMethod.Uri)) throw new DnsException("Uri is null or empty");
+            if (HttpsRequired && !uriMethod.Uri.ToLower().StartsWith("https://"))
+                throw new DnsException($"configuration require https uri but it is not, current: '{uriMethod.Uri}'");
+
+
         }
 
-        public static DnsClientMessageIODoHRfc8484 CreateDefault(string uri)
+        public static DnsClientMessageIODoHRfc8484 CreateDefault(Func<DnsClientMessageIOArg, UriMethodResult> uriMethodSelect)
         {
-            return new DnsClientMessageIODoHRfc8484(uri);
+            return new DnsClientMessageIODoHRfc8484(uriMethodSelect);
+        }
+
+        public struct UriMethodResult
+        {
+            public string Uri;
+            public string Method;
         }
     }
 }
